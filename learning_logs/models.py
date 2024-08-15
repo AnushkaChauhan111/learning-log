113266441d178f810555ac150e5b35a7e7d3be64
Anushka111 <anushkachauhan69532@gmail.com>
2024-08-15 22:39:14 +0530

    Added code to model django

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    text = models.CharField(max_length = 100)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
