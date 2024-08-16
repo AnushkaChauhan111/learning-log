a0d5d7fa8ee8cb8d1eef9b488e1de566d521200d
Anushka111 <anushkachauhan69532@gmail.com>
2024-08-16 06:30:58 +0530

    Accessing through superuser and registered with admin site

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Topic #(.models to ask it to find models in same directory as admin)
+admin.site.register(Topic)
